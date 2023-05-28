f6eeba96ecbbf610d836486436584ba9889abb29
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-05-28 16:59:10 +0200

    Show example with vanilla extract

---
+++ src/components/Button/Button.css.ts
@@ -0,0 +1,10 @@
+import { style } from "../../styles";
+
+export const button = style({
+  backgroundColor: "green",
+  color: "white",
+  border: "none",
+  borderRadius: "7px",
+  padding: "0.75rem 1rem",
+  textTransform: "uppercase",
+});
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,25 +1,8 @@
+import { button } from "./Button.css";
 interface Props {
-  /**
-   * Button label
-   */
   children: string;
 }
 export default function Button(props: Props) {
   const { children } = props;
-  return (
-    <button
-      style={{
-        backgroundColor: "hotpink",
-        border: "none",
-        color: "#333",
-        borderRadius: 7,
-        textTransform: "uppercase",
-        fontFamily: "sans-serif",
-        padding: "0.75rem 1rem",
-        cursor: "pointer",
-      }}
-    >
-      {children}
-    </button>
-  );
+  return <button className={button}>{children}</button>;
 }
